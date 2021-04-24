c28f120e711316d79892a38d895fc714a33118e9
Stephanie Norton <steph.jane.norton@gmail.com>
2021-04-23 19:11:26 -0600

    Create header with basic layout

--- App.js
+++ App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { Header } from 'react-native-elements';
 import Main from './app/components/Main'
 
 export default function App() {
   return (
-    <Main />
+    <SafeAreaProvider>
+      <Header
+        placement="center"
+        leftComponent={{ icon: 'menu', color: '#fff' }}
+        centerComponent={{ text: 'Pet Name Finder', style: { color: '#FCA311', fontSize: 28, fontFamily: 'Comic Sans MS, Comic Sans, cursive' } }}
+        rightComponent={{ icon: 'home', color: '#fff' }}
+        containerStyle={{
+          backgroundColor: '#14213D',
+        }}
+      />
+      <Main />
+    </SafeAreaProvider>
   );
 }
 
