13267207506e7455732a45bb7cb6a2123d66d060
Stephanie Norton <steph.jane.norton@gmail.com>
2021-04-30 10:15:42 -0600

    Create new style file

---
+++ app/styles/Styles.js
@@ -0,0 +1,29 @@
+
+import { StyleSheet } from 'react-native';
+
+export const STYLES = StyleSheet.create({
+  flex: {
+    flex: 1
+  },
+  centerContainer: {
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  title: {
+    fontSize: 22
+  },
+  // add below
+  selectButtonContainer: {
+    margin: 20,
+    borderRadius: 5
+  },
+  selectButtonTitle: {
+    padding: 10,
+    fontSize: 18
+  }
+});
+
+export const COLORS = {
+  primaryDark: '#22212c',
+  primaryLight: '#f8f8f2',
+};
