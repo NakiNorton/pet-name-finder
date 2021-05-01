2797f67f17b36691dadba5478e6070a8c4fb3e8e
Stephanie Norton <steph.jane.norton@gmail.com>
2021-04-30 18:00:04 -0600

    Import mockData and create basic structure for data handling

--- app/components/App.js
+++ app/components/App.js
@@ -1,23 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { Header } from 'react-native-elements';
 import Main from './Main'
+import { mockNameData } from '../mockdata'
 import { Constants } from 'react-native-unimodules';
-console.log("constants",  Constants.systemFonts);
+// console.log("constants",  Constants.systemFonts);
 
 export default function App() {
+  const [ allNames, setAllNames ] = useState([])
+
+  useEffect(() => {
+    // fetch data
+    const allNames = mockNameData
+    setAllNames(allNames)
+  })
+
   return (
     <SafeAreaProvider>
       <Header
         placement="center"
         leftComponent={{ icon: 'menu', color: '#fff' }}
-        centerComponent={{ text: 'Pet Name Finder', style: { color: '#FCA311', fontSize: 28, fontFamily: "Avenir-Light" } }}
+        centerComponent={{ text: 'Pet Name Finder', style: { color: '#FCA311', fontSize: 26, fontFamily: "Avenir-Light" } }}
         rightComponent={{ icon: 'home', color: '#fff' }}
         containerStyle={{
           backgroundColor: '#14213D',
         }}
       />
-      <Main />
+      <Main allNames={allNames} />
     </SafeAreaProvider>
   );
 }
